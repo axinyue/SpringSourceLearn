1e639a8721119ede20496f2422a4a77ebf7868ed
axinyue <xukui@app77.cn>
2020-11-19 19:24:36 +0800

    run a sql

--- src/main/java/com/ax/mysql/Test1.java
+++ src/main/java/com/ax/mysql/Test1.java
@@ -1,9 +1,6 @@
 package com.ax.mysql;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Test1 {
 
@@ -16,19 +13,27 @@ public class Test1 {
      * @author ax. 20201119  <br>
      * link: https://dev.mysql.com/doc/connector-j/5.1/en/ <br>
      */
-    public static void f1(){
-        try {
-            // Load Driver
-            Class.forName("com.mysql.jdbc.Driver");
+    public static Connection getConnection() throws SQLException,ClassNotFoundException{
+        Class.forName("com.mysql.jdbc.Driver");
+        // get connection
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=minty&password=greatsqldb");
+        return connection;
+    }
 
-            // get connection
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=minty&password=greatsqldb");
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+    /**
+     * Why need Statement? How work is?
+     * 1. You can use Statement execute a sql. may be is select or update. in Statement only use method executeQuery or executeUpdate to run sql and get result.  <br>
+     *
+     * How to get a Object from result?
+     * parse? or other.
+     *
+     * @throws Exception
+     */
+    public static void firstQuery() throws Exception{
+        Connection connection = getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from bar");
+        int result = statement.executeUpdate("update bar set id=1 where id =1");
 
     }
 }
